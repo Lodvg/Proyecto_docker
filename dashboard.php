4b8c9e27eac420acf41485f23b4414547ce4f14a
Ada <u00032741@sise.com.pe>
2026-06-19 03:11:55 +0000

    fix: agregar boton Cerna al dashboard

--- dashboard.php
+++ dashboard.php
@@ -249,6 +249,7 @@ $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <a href="dashboard.php?filtro=activos"   class="btn btn-filtro <?= $filtro === 'activos'   ? 'activo' : '' ?>">Activos</a>
         <a href="dashboard.php?filtro=inactivos" class="btn btn-filtro <?= $filtro === 'inactivos' ? 'activo' : '' ?>">Inactivos</a>
         <a href="#" class="btn btn-filtro">Jaime</a>
+       <a href="#" class="btn btn-filtro">Cerna</a>
     </div>
 
     <table>
