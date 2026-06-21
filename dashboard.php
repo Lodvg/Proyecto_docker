6c3e15343aa934780f0e033392c1885d4a2e1b99
Antony <u00031170@sise.com.pe>
2026-06-20 19:18:02 -0500

    boton Campos

--- dashboard.php
+++ dashboard.php
@@ -248,7 +248,6 @@ $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <a href="dashboard.php?filtro=todos"     class="btn btn-filtro <?= $filtro === 'todos'     ? 'activo' : '' ?>">Todos</a>
         <a href="dashboard.php?filtro=activos"   class="btn btn-filtro <?= $filtro === 'activos'   ? 'activo' : '' ?>">Activos</a>
         <a href="dashboard.php?filtro=inactivos" class="btn btn-filtro <?= $filtro === 'inactivos' ? 'activo' : '' ?>">Inactivos</a>
-        <a href="#" class="btn btn-filtro">Jaime</a>
 	<a href="#" class="btn btn-filtro">Campos</a>
     </div>
 
